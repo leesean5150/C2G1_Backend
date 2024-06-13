0e528c24741797bcb5c7d39e58a07d6bfecd9289
leesean5150 <sean.lee@nexusfrontier.tech>
2024-06-13 11:49:54 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# ReactNodeProject
+A project running a dockerized Node.js backend and MongoDB database for Dell.
+
+## Requirements
+- docker
+
+## Getting started
+
+### Clone repository
+
+```shell
+git clone https://github.com/leesean5150/C2G1_Backend.git
+```
+
+### Setting Up Configuration
+Create a new .env file for the server folder using the .env.sample as a tempalte. For email reset link, navigate to Google Account settings, search for app passwords and create a new app. The password is the password to be put into the .env file.
+
+### Run Server
+
+```shell
+docker compose up --build
+```
+
+Open [http://localhost:5000](http://localhost:5000) to connect to backend api endpoints.
---
+++ src/app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from "cors";
+import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
+
+import config from "../config.js";
+import productRoutes from './routes/products.js';
+import { UserRouter } from './routes/user.js';
+
+const connectToDB = async () => {
+    try {
+        await mongoose.connect(config.db_uri, {})
+    }catch (e) {
+        console.log(e);
+        process.exit(1)
+    }
+}
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+  origin: [process.env.FRONT_END_URL],
+  credentials: true,
+}));
+app.use(cookieParser());
+
+app.use('/products', productRoutes);
+app.use('/auth', UserRouter);
+
+await connectToDB();
+
+export default app;
