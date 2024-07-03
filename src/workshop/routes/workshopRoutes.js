526228bbf2fc38bb3b8956e67d03beb7e28f130e
JunJin1218 <jin_jun@mymail.sutd.edu.sg>
2024-07-03 17:53:27 +0800

    minor fix
    /get [POST -> GET]

--- src/workshop/routes/workshopRoutes.js
+++ src/workshop/routes/workshopRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post('/', WorkshopController.createWorkshop);
 router.get('/', WorkshopController.getAllWorkshops);
-router.post('/get', WorkshopController.getWorkshop);
+router.get('/get', WorkshopController.getWorkshop);
 router.delete('/del', verifyAdmin, WorkshopController.deleteWorkshop); // verifyAdmin
 
 export { router as WorkshopRouter };
