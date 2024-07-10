c113fbcac8e89a18fdbd7cbfd3c91f187bde0f3b
Jengkuen <66157501+Jengkuen@users.noreply.github.com>
2024-07-11 00:57:34 +0800

    removed logging and comments

--- src/middlewares/updateUnavailableTimeslots.js
+++ src/middlewares/updateUnavailableTimeslots.js
@@ -5,7 +5,7 @@ import { Workshop } from "../workshop/models/Workshop.js";
  * Middleware to update a trainer's unavailable timeslots.
  */
 const updateUnavailableTimeslotsMiddleware = async (req, res, next) => {
-  const trainerId = req.body.trainerId; // Accessing trainerId from the request body
+  const trainerId = req.body.trainerId;
 
   try {
     const trainer = await Trainer.findById(trainerId);
@@ -13,30 +13,18 @@ const updateUnavailableTimeslotsMiddleware = async (req, res, next) => {
       return res.status(404).json({ message: "Trainer not found" });
     }
 
-    console.log(trainerId);
-
-    // Fetch all workshops the trainer is assigned to
     const workshops = await Workshop.find({ trainers: trainerId });
-    console.log(workshops);
 
-    // Create new array to store unavailable timeslots
     const newUnavailableTimeslots = [];
 
-    // Loop through all workshops the trainer is assigned to
     workshops.forEach((workshop) => {
-      // Construct the timeslot object
       const newTimeslot = {
         start: workshop.startDate,
         end: workshop.endDate,
       };
-      // Push the timeslot to the array
       newUnavailableTimeslots.push(newTimeslot);
-      console.log(newTimeslot);
     });
 
-    console.log(newUnavailableTimeslots);
-
-    // Update the trainer's unavailableTimeslots
     trainer.unavailableTimeslots = newUnavailableTimeslots.map((timeslot) => ({
       start: timeslot.start,
       end: timeslot.end,
@@ -44,7 +32,6 @@ const updateUnavailableTimeslotsMiddleware = async (req, res, next) => {
 
     await trainer.save();
 
-    // Send response to client
     return res.status(200).json({ message: "Timeslots updated successfully" });
   } catch (error) {
     console.error(error);
