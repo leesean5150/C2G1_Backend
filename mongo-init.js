a073e54dc373fa90c0164bd0c94807bc91a81827
Jengkuen <66157501+Jengkuen@users.noreply.github.com>
2024-07-17 13:07:11 +0800

    added mongo-init.js. edited dockercompose to run script.

---
+++ mongo-init.js
@@ -0,0 +1,78 @@
+db = db.getSiblingDB("docker");
+
+db.clients.insertMany([
+  {
+    username: "client12345",
+    email: "client123@client.com",
+    fullname: "Client",
+    password: "$2a$10$RL7CzLXMEaIqbkV9SwkRQeK5zLMKyggBboxayQnHbSBuCHksGoOjS",
+    country: "Singapore",
+    role: "client",
+    workshop: [],
+  },
+]);
+
+// db.workshopsummaries.insertMany([
+//   {
+//     trainer_data: [
+//       {
+//         workshop_name: "Intro to Python",
+//         workshop_ID: "01",
+//         workshop_type: "Business Value Discovery",
+//         workshop_details: "Nothing to do with the snake.",
+//         workshop_availability: "Available",
+//       },
+//       {
+//         workshop_name: "Intro to Java 21",
+//         workshop_ID: "02",
+//         workshop_type: "Business Value Discovery",
+//         workshop_details: "No its not about Indonesia.",
+//         workshop_availability: "Available",
+//       },
+//       {
+//         workshop_name: "Intro to AI",
+//         workshop_ID: "03",
+//         workshop_type: "AI Platform",
+//         workshop_details: "hello world!",
+//         workshop_availability: "Available",
+//       },
+//       {
+//         workshop_name: "Intro to C Hashtag",
+//         workshop_ID: "04",
+//         workshop_type: "Infrastructure and Demo",
+//         workshop_details:
+//           "Those saying its C sharp don't know what they're talking about...",
+//         workshop_availability: "Available",
+//       },
+//       {
+//         workshop_name: "Intro to Python",
+//         workshop_ID: "01",
+//         workshop_type: "Business Value Discovery",
+//         workshop_details: "Nothing to do with the snake.",
+//         workshop_availability: "Available",
+//       },
+//       {
+//         workshop_name: "Intro to Java 21",
+//         workshop_ID: "02",
+//         workshop_type: "Business Value Discovery",
+//         workshop_details: "No its not about Indonesia.",
+//         workshop_availability: "Available",
+//       },
+//       {
+//         workshop_name: "Intro to AI",
+//         workshop_ID: "03",
+//         workshop_type: "AI Platform",
+//         workshop_details: "hello world!",
+//         workshop_availability: "Available",
+//       },
+//       {
+//         workshop_name: "Intro to C Hashtag",
+//         workshop_ID: "04",
+//         workshop_type: "Infrastructure and Demo",
+//         workshop_details:
+//           "Those saying its C sharp don't know what they're talking about...",
+//         workshop_availability: "Available",
+//       },
+//     ],
+//   },
+// ]);
