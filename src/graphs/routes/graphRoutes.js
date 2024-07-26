4a9b8115e2914000eb680b4453991de19bee2e72
Clarencelaujw <clarencelaujw@gmail.com>
2024-07-27 00:47:25 +0800

    chore: Add temporary routes for workshop trend data and pie chart data

--- src/graphs/routes/graphRoutes.js
+++ src/graphs/routes/graphRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import graphController from "../Controllers/graphController.js";
+import temp from "../Controllers/temp.js";
 
 const router = express.Router();
 
@@ -8,5 +9,12 @@ router.get("/getTrainerGraph", graphController.getTrainerGraph);
 router.get("/getTodayGraph", graphController.getTodayGraph);
 router.get("/getYearsPieChartGraph", graphController.getYearsPieChartGraph);
 router.get("/getTotalPieChartGraph", graphController.getTotalPieChartGraph);
+
+router.get("/getWorkshopTrendDataGraph", temp.getWorkshopTrendDataGraph);
+router.get("/getHCtotalPieData", temp.getHCtotalPieData);
+router.get("/getHCyearsPieData", temp.getHCyearsPieData);
+router.get("/getHCWorkshopTypesData", temp.getHCWorkshopTypesData);
+router.get("/getHCClientTypesData", temp.getHCClientTypesData);
+router.get("/getHCWorkshopTrendData", temp.getHCWorkshopTrendData);
 
 export { router as graphRouter };
