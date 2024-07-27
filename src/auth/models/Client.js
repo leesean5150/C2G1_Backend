c112896d91e8ca9479e255428d3a72338547b4f2
leesean5150 <sean.lee@nexusfrontier.tech>
2024-07-27 12:27:15 +0800

    removed required flag on client model to prevent errors with initial client accounts creating workshop requests

--- src/auth/models/Client.js
+++ src/auth/models/Client.js
@@ -8,7 +8,7 @@ const clientSchema = new mongoose.Schema({
   password: { type: String, required: true },
   country: { type: String, required: true },
   role: { type: String, default: "client" },
-  client_type: { type: String, required: true },
+  client_type: { type: String },
   workshop_request: [
     {
       type: mongoose.Schema.Types.ObjectId,
